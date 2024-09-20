d15bead194266c2b112b4d6ee299772c513aee63
mathias-villena <mathias.villena@tecsup.edu.pe>
2024-09-19 19:53:01 -0500

    agregando manejo de lista de tareas

--- agregar-tarea.php
+++ agregar-tarea.php
@@ -1,3 +1,16 @@
 <?php
 session_start();
+//array asociativo (variable de session)
+if (!isset($_SESSION['tareas'])) //el if verifica si la variable de session tareas existe(si no existe la creas)
+{
+    $_SESSION['tareas']=[];
+}
+
+$tareas=$_SESSION['tareas']; //array local
+
+$nueva_tarea=$_POST['tarea'];//jalando del formulario
+
+$tareas[]=$nueva_tarea;
+$_SESSION['tareas']=$tareas;
+header('Location:index.php');
 ?>
